1883f52cdfab0bcfd1b8662006f0f7bd096e077e
Istvan Soos <istvan.soos@gmail.com>
2016-09-18 18:02:12 +0000

    Add settings and generate sitemap.

--- lib/tavern.dart
+++ lib/tavern.dart
@@ -2,22 +2,29 @@ library tavern;
 
 import 'package:barback/barback.dart';
 
+import 'package:tavern/src/cleanup.dart';
 import 'package:tavern/src/markdown.dart';
 import 'package:tavern/src/metadata.dart';
 import 'package:tavern/src/metadata_aggregator.dart';
-import 'package:tavern/src/template.dart';
-import 'package:tavern/src/cleanup.dart';
+import 'package:tavern/src/settings.dart';
+import 'package:tavern/src/sitemap.dart';
 import 'package:tavern/src/tag_index.dart';
 import 'package:tavern/src/tag_pages.dart';
+import 'package:tavern/src/template.dart';
 import 'package:tavern/src/template_cleanup.dart';
 
 class Tavern implements TransformerGroup {
-  final Iterable<Iterable> phases;
-  Tavern() : phases = createPhases();
-  Tavern.asPlugin(settings) : this();
+  @override
+  Iterable<Iterable> phases;
+
+  Tavern.asPlugin(BarbackSettings settings) {
+    phases = createPhases(settings);
+  }
 }
 
-List<List<Transformer>> createPhases() {
+List<List<Transformer>> createPhases(BarbackSettings barbackSettings) {
+  TavernSettings settings =
+      new TavernSettings.fromMap(barbackSettings.configuration);
   return [
     [new Metadata()],
     [new TagIndex()],
@@ -26,6 +33,7 @@ List<List<Transformer>> createPhases() {
     [new Template()],
     [new TemplateCleanup()],
     [new TagPages()],
+    [new Sitemap(settings)],
     [new Cleanup()],
   ];
 }
